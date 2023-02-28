7aed4226e57db5eaa07c8df3591aec2536cffe2d
Lemos0250 <vitorlemos703@gmail.com>
2023-02-27 22:25:57 -0300

    Primeiro Commit

---
+++ naosecale/src/pages/App.js
@@ -0,0 +1,18 @@
+import { Box } from '../components/Box'
+
+export default function App() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center bg-slate-50 text-red-600">
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+    </div>
+  );
+}
+
