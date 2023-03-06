c91d6089d5563dbec72ab1b01297b22fca3d96d7
Lemos0250 <vitorlemos703@gmail.com>
2023-03-06 09:25:33 -0300

    Inicio

--- naosecale/src/pages/App.js
+++ naosecale/src/pages/App.js
@@ -2,17 +2,43 @@ import { Box } from '../components/Box'
 
 export default function App() {
   return (
-    <div className="flex w-full h-screen justify-center items-center bg-slate-50 text-red-600">
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
-      <Box />
+    <div class="geral">
+    <div class="text-base flex flex-row items-center justify-between pl-1 pr-5 pt-7 pb-7 shadow-xl text-red-600">
+      <div class="hidden flex-row md:flex ml-20">
+
+        <div class="italic font-extrabold flex flex-row items-center ml-20 text-2xl">
+            <span>Não Se Cale !</span>
+        </div>
+        <div class="underline flex flex-row items-center pl-20">
+            <span>Início</span>
+        </div>
+        <div class="underline flex flex-row items-center pl-6">
+            <span>Sobre</span>
+        </div>
+        <div class="underline flex flex-row items-center pl-6">
+            <span>Postar</span>
+        </div>
+      </div>
     </div>
+        <div class="w-full h-full justify-center items-center align-middle">
+          <div class="flex flex-col w-full lg:w-[45%] pt-14 lg:pt20 pl-20 items-center">
+            <a class="italic font-extrabold text-2xl pb-5">Lorem ipsum</a>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
+          </div>
+          <div class="flex flex-col w-full lg:w-[45%] pt-14 lg:pt20 pl-20 items-center">
+            <a class="italic font-extrabold text-2xl pb-5">Lorem ipsum</a>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
+          </div>
+          <div class="flex flex-col w-full lg:w-[45%] pt-14 lg:pt20 pl-20 items-center">
+            <a class="italic font-extrabold text-2xl pb-5">Lorem ipsum</a>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</span>
+          </div>
+          
+
+        </div>
+    </div>
+  
+    
   );
 }
 
