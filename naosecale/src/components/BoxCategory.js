4c3596de165e8cfe42423050f12de9fd48040d7f
Lemos0250 <vitorlemos703@gmail.com>
2023-04-17 22:23:12 -0300

    Login Register

--- naosecale/src/components/BoxCategory.js
+++ naosecale/src/components/BoxCategory.js
@@ -1,6 +1,6 @@
 export function BoxCategory(){
     return(
-        <div className="flex flex-col text-2xl border-2 border-[#6b0023] p-full">
+        <div className="flex flex-col text-2xl border-2 justify-start items-start border-[#6b0023] p-full px-[100%]">
             <span>Violência contra a Mulher</span>
             <a className="text-sm">#Diga não</a>
         </div>
--- naosecale/src/pages/login.js
+++ naosecale/src/pages/login.js
@@ -12,22 +12,31 @@ export function Login(){
                 <div className="flex flex-row h-screen lg:flex-row items-center justify-center shadow-xl bg-[#FFDEF6]">
                     <div className="flex flex-col justify-init items-center bg-white rounded-lg lg:h-[65%] lg:w-[35%] lg:mt-[-10%]">
 
-                        <div className="italic font-extrabold text-3xl text-[#6b0023] lg:mt-[8%]">
+                        <div className="italic font-extrabold text-3xl text-[#6b0023] lg:mt-[10%]">
                             <span>Não se Cale !</span>
                         </div>
 
                         <div className="text-ml justify-start items-center">
                             <div>
-                                <textarea placeholder="Email ou Telefone" className="lg:mt-[40%] lg:w-[200%] lg:ml-[-60%] border-2 rounded-lg"/> 
+                                <textarea placeholder="....Email ou Telefone" className="lg:mt-[20%] lg:w-[210%] lg:ml-[-60%] border-2 rounded-lg"/> 
                             </div>
 
                             <div>
-                                <textarea placeholder="Senha" className="lg:mt-[10%] lg:w-[200%] lg:ml-[-60%] border-2 rounded-lg"/> 
+                                <textarea placeholder="....Senha" className="lg:mt-[5%] lg:w-[210%] lg:ml-[-60%] border-2 rounded-lg"/> 
                             </div>
 
-                            <div className=''>
+                            <div className="mt-[15%] text-center border-solid border-2 rounded-lg p-2 bg-[#a6024f] text-white text-2xl lg:ml-[-20%]">
                                 <span>Entrar</span>
                             </div>
+
+                            <div className="text-md text-black lg:my-[5%]">
+                                <span>Esqueceu a Conta ?</span>
+                            </div>
+
+                            <div className="mt-[5%] text-center border-solid border-2 rounded-lg p-2 bg-[#a6024f] text-white text-2xl lg:ml-[-20%]">
+                                <span>Criar uma Conta</span>
+                            </div>
+
                         </div>
 
                     </div>
