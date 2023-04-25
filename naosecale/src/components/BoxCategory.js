e431963f9539ee5ede57cf524c312ecbd1f89330
Lemos0250 <vitorlemos703@gmail.com>
2023-04-24 21:53:58 -0300

    Mobile

--- naosecale/src/components/BoxCategory.js
+++ naosecale/src/components/BoxCategory.js
@@ -1,6 +1,6 @@
 export function BoxCategory(){
     return(
-        <div className="flex flex-col text-2xl border-2 justify-start items-start border-[#6b0023] p-full px-[100%]">
+        <div className="flex flex-col w-full text-2xl border-2 p-2 items-start border-[#6b0023]">
             <span>Violência contra a Mulher</span>
             <a className="text-sm">#Diga não</a>
         </div>
--- naosecale/src/pages/contact.js
+++ naosecale/src/pages/contact.js
@@ -8,39 +8,37 @@ export function Contact() {
         <>
             <div>
                 <Topbar/>
-                <div className="flex flex-col lg:flex-row items-center justify-between shadow-xl bg-[#FFDEF6]">
+                <div className="flex flex-col lg:flex-row items-center justify-center shadow-xl bg-[#FFDEF6]">
         
                     <div className="border-solid shadow-xl lg:m-96 lg:my-14 bg-white rounded-lg">
 
-                        <div className="flex flex-col lg:flex-row lg:m-20 lg:ml-56 lg:mt-15 text-4xl">
+                        <div className="flex flex-col lg:flex-row items-center lg:m-20 lg:ml-56 lg:mt-15 text-4xl mt-[10%]">
                             <span>Contato</span>
                             <input type="text" className="hidden lg:flex shadow-lg border-2 hover:border-pink-500 ml-20 rounded-full text-black transition-colors margin lg:ml-20 lg:w-60 lg:ml-10 text-2xl"/>
                         </div>
             
-                        <div className="lg:m-20 lg:mt-30 lg:mb-5 text-center">
-                            <a>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999<br></br>
-                            </a>
+                        <div className="flex flex-col items-center lg:m-20 lg:mt-30 lg:mb-5 text-center mt-[15%]">
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
+                            <a href="/contacts">Lorem Ipsum Av.Lorem Ipsum........................(99)99999-9999</a>
                         </div>
                         <div className="text-center items-center gap-3 flex flex-row ml-[45%] my-6">
                             <span>1 2 3 4...</span>
