90130dfba661363cfedf74573e95f01e4b81300b
Lemos0250 <vitorlemos703@gmail.com>
2023-05-08 23:13:36 -0300

    Api fail

--- naosecale/src/pages/login.js
+++ naosecale/src/pages/login.js
@@ -3,10 +3,24 @@ import { Link } from "react-router-dom";
 import { Box } from '../components/Box'
 import { Footer } from '../components/Footer'
 import { Topbar } from '../components/Topbar'
+import { useState } from 'react'
+import axios from 'axios'
+
+export function Login() {
+    const { email, setEmail } = useState()
+    const { password, setPassword } = useState()
+
+    async function signIn() {
+        const response = await axios.post('https://naosecalemulher.azurewebsites.net/api/Autenticacao/v1/signin', {
+            email,
+            password
+        })
+
+        console.log(response)
+    }
 
 
-export function Login(){
-    return(
+    return (
         <>
             <div className="bg-black">
 
@@ -18,30 +32,30 @@ export function Login(){
                         </div>
 
                         <div className="text-ml justify-center items-center gap-2">
-                
-                            <div className="flex flex-col lg:flex-row w-full gap-5">
-                                <input type='text' placeholder="Nome" className="lg:w-[50%] border-2 rounded-lg p-2"/> 
 
-                                <input type='text' placeholder="Sobrenome" className="lg:w-[50%] border-2 rounded-lg p-2"/> 
+                            <div className="flex flex-col lg:flex-row w-full gap-5">
+                                <input type='text' placeholder="Email" onChange={((e) => setEmail(e.target.value))} className="lg:w-[50%] border-2 rounded-lg p-2" />
+
+                                <input type='text' placeholder="Senha" onChange={((e) => setPassword(e.target.value))} className="lg:w-[50%] border-2 rounded-lg p-2" />
                             </div>
 
-                        <Link to="/">
-                            <div className="mt-[15%] text-center border-solid border-2 rounded-lg p-2 bg-[#a6024f] text-white text-2xl">
+                            {/* <Link to="/"> */}
+                            <div onClick={signIn} className="mt-[15%] text-center border-solid border-2 rounded-lg p-2 bg-[#a6024f] text-white text-2xl">
                                 <span>Entrar</span>
                             </div>
-                        </Link>
+                            {/* </Link> */}
 
-                        <Link to="https://www.youtube.com/watch?v=SKg-WkkBCfM">
-                            <div className="text-md text-black lg:my-[5%] text-center">
-                                <button>Esqueceu a Conta ?</button>
-                            </div>
-                        </Link>
+                            <Link to="https://www.youtube.com/watch?v=SKg-WkkBCfM">
+                                <div className="text-md text-black lg:my-[5%] text-center">
+                                    <button>Esqueceu a Conta ?</button>
+                                </div>
+                            </Link>
 
-                        <Link to="/register">
-                            <div className="text-center border-solid border-2 rounded-lg p-2 bg-[#a6024f] text-white text-2xl">
-                                <span>Criar uma Conta</span>
-                            </div>
-                        </Link>
+                            <Link to="/register">
+                                <div className="text-center border-solid border-2 rounded-lg p-2 bg-[#a6024f] text-white text-2xl">
+                                    <span>Criar uma Conta</span>
+                                </div>
+                            </Link>
 
                         </div>
 
@@ -50,7 +64,7 @@ export function Login(){
                 </div>
 
             </div>
-            <Footer/>
+            <Footer />
         </>
     )
 }
--- naosecale/src/pages/register.js
+++ naosecale/src/pages/register.js
@@ -1,12 +1,28 @@
 import { Box } from '../components/Box'
 import { Footer } from '../components/Footer'
 import { Topbar } from '../components/Topbar'
+import axios from 'axios'
 
 
-export function Register(){
-    return(
+
+export function Register() {
+
+    async function register() {
+        const response = await axios.post('http://localhost:5005/api/Autenticacao/v1/registe', {
+            nomeCompleto: 'string',
+            email: 'string',
+            senha: 'string',
+            apelido: 'string'
+        })
+
+        console.log(response)
+
+    }
+
+    return (
         <div>
             <div className="bg-black">
+                <button onClick={register}>Teste</button>
 
                 <div className="flex flex-row h-screen lg:flex-row items-center justify-center shadow-xl bg-[#FFDEF6]">
                     <div className="flex flex-col justify-center items-center bg-white rounded-lg lg:h-[75%] lg:w-[50%] w-full">
@@ -17,27 +33,27 @@ export function Register(){
 
                         <div className=" flex flex-col w-[50%] justify-start items-center lg:mt-[1%] mt-[10%] gap-5 py-10">
                             <div className="flex flex-col lg:flex-row w-full gap-5">
-                                <input type='text' placeholder="Nome" className="lg:w-[50%] border-2 rounded-lg p-2"/> 
+                                <input type='text' placeholder="Nome" className="lg:w-[50%] border-2 rounded-lg p-2" />
 
-                                <input type='text' placeholder="Sobrenome" className="lg:w-[50%] border-2 rounded-lg p-2"/> 
+                                <input type='text' placeholder="Sobrenome" className="lg:w-[50%] border-2 rounded-lg p-2" />
                             </div>
 
                             <div className="w-full lg:mt-[5%]">
-                                <input type='email' placeholder="Email" className="w-full border-2 rounded-lg p-2"/> 
+                                <input type='email' placeholder="Email" className="w-full border-2 rounded-lg p-2" />
                             </div>
 
                             <div className="w-full lg:mt-[2%]">
-                                <input type='password' placeholder="Nova Senha" className="w-full border-2 rounded-lg p-2"/> 
+                                <input type='password' placeholder="Nova Senha" className="w-full border-2 rounded-lg p-2" />
                             </div>
-                            
-                            <div className= "flex flex-col w-full justify-start items-start lg:mt-[2.5%]">
+
+                            <div className="flex flex-col w-full justify-start items-start lg:mt-[2.5%]">
                                 <span>Data de nascimento</span>
 
                                 <div className=" lg:h-[100%] ">
-                                    <input type="date" placeholder="Paloma" className="w-full border-2 rounded-lg p-2"/> 
+                                    <input type="date" placeholder="Paloma" className="w-full border-2 rounded-lg p-2" />
                                 </div>
                             </div>
-                            
+
                             <div className="flex flex-col w-full lg:h-[100%] mb-[5%]">
                                 <span>Gênero</span>
                                 <div className="border-2 p-2 w-[80%] border-[#6b0023] rounded-md">
@@ -56,7 +72,7 @@ export function Register(){
                 </div>
 
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
