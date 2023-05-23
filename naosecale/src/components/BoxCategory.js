29ff6863df69c0226a027681f59990808766424e
arthurcant <arthurbig19@gmail.com>
2023-05-22 22:16:19 -0300

    atualizaç]

--- naosecale/src/components/BoxCategory.js
+++ naosecale/src/components/BoxCategory.js
@@ -1,8 +1,8 @@
-export function BoxCategory(){
+export function BoxCategory(props){
     return(
         <div className="flex flex-col w-full text-2xl border-2 p-2 items-start border-[#6b0023]">
-            <span>Violência contra a Mulher</span>
-            <a className="text-sm">#Diga não</a>
+            <span>{props.nomeCategoria}</span>
+            <a className="text-sm">{props.nomeTag}</a>
         </div>
     )
 }
--- naosecale/src/pages/categories.js
+++ naosecale/src/pages/categories.js
@@ -4,9 +4,52 @@ import { SearchBar } from '../components/SearchBar'
 import { Footer } from '../components/Footer'
 import { BoxCategory } from '../components/BoxCategory'
 import { Pagination } from '../components/Pagination'
-
+import api  from '../services/api'
+import React, { useState, useEffect} from 'react'
 
 export function Categories(){
+    
+    const [currentPage, setCurrentPage] = useState(0);
+    const [amountPages, setAmountPages] = useState(0);
+    const [totalResults, setTotalResults] = useState(0);
+    const [pageSize, setPageSize] = useState(4);
+    const [listCategorias, setListPosters] = useState([]);
+    const [listNum, setListNum] = useState([]);
+    const [page, setPage] = useState(1);
+    const email = localStorage.getItem('email')
+    const accessToken = localStorage.getItem('accessToken')
+
+    const authorization = {
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    }
+
+    useEffect(() => {
+        fetchMorePosters()
+    }, [accessToken])
+    
+    async function fetchMorePosters(numPage = 1) {
+        const response = await api.get(`/api/categorias/v1/asc/${pageSize}/${numPage}`, authorization)
+
+        console.log(response)
+
+        setPageSize(response.data.pageSize)
+        setTotalResults(response.data.totalResults)
+        setCurrentPage(response.data.currentPage)
+        setListPosters([...response.data.list])
+        setAmountPages((totalResults % 2 == 0 ? totalResults / pageSize : (totalResults / pageSize) + 1 ))
+    }
+    
+    function calcNumPage() {
+        var td = [];
+        for (let index = 1; index <= (totalResults % 2 == 0 ? totalResults / pageSize : (totalResults / pageSize) + 1 ); ++index) {
+            td.push(index)
+        }
+        return td;
+    }
+
+
     return(
             <div>
                 <Topbar/>
@@ -23,11 +66,19 @@ export function Categories(){
                                 <SearchBar/>
 
                                 <div className="flex flex-col w-[80%] justify-center items-center gap-3 lg:mt-[2.5%] mt-[5%]">
-                                    <BoxCategory />
-                                    <BoxCategory />
-                                    <BoxCategory />
-                                    <BoxCategory />
-                                    
+                                {listCategorias.map((categoria) => (
+                                    <BoxCategory 
+                                    id={categoria.id} 
+                                    nomeCategoria={categoria.nomeCategoria} 
+                                    nomeTag={categoria.nomeTag}
+                                    />
+                                ))}  
+
+                                {calcNumPage().map((element, index) => 
+                                    <div key={index}>
+                                        <button onClick={() => fetchMorePosters(element)}>{element}</button>
+                                    </div>
+                                )}
 
                                     <div className="lg:mt-[2.5%]"><Pagination/></div>
                                 </div>
